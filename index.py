39c719a17152e453fb309b8c3de8120220694abc
Araf <67403822+Sadman-Hasin@users.noreply.github.com>
2023-03-14 01:32:13 +0600

    Update index.py

--- index.py
+++ index.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def index():
     return "<p>Server Running...</p>"
 
-client = HappyClasher(os.getenv("EMAIL"), os.getenv("PASSWORD"))
-client.listen()
+#client = HappyClasher(os.getenv("EMAIL"), os.getenv("PASSWORD"))
+#client.listen()

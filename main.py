220759698fc0f88ab7005c68d98d5193e4b634a7
Araf <67403822+Sadman-Hasin@users.noreply.github.com>
2023-03-13 21:50:44 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from bot import HappyClasher
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "<p>Server Running...</p>"
+
+client = HappyClasher(os.getenv("EMAIL"), os.getenv("PASSWORD"))
+client.listen()

d80f919e8ff13579aa6e6810b29b6ea1fee25367
Araf <67403822+Sadman-Hasin@users.noreply.github.com>
2023-03-14 11:29:31 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,14 @@ class HappyClasher(Client):
                 thread_type=thread_type,
             )
         
-        
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "<p>Server Running...</p>"
+  
 client = HappyClasher(os.getenv("EMAIL"), os.getenv("PASSWORD"))
 client.listen()
 

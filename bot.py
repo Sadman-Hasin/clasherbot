d1cf5fa155fc436b8abdd4ed9a8ad45dc33ee770
Araf <67403822+Sadman-Hasin@users.noreply.github.com>
2023-03-14 11:52:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,9 +56,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "<p>Started Running...</p>"
     client = HappyClasher(os.getenv("EMAIL"), os.getenv("PASSWORD"))
     client.listen()
+    return "<p>Started Running...</p>"
 
 
 
